55f044a566dfd7e2aa9972057d79f866996dc622
Adavletbaev <alex.davletbaev@yandex.ru>
2021-12-09 20:55:28 +0300

    ver.3

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 20;
+        int price = 20;
+        int bonusMile;
+        bonusMile = ticket / price;
+        System.out.println("Your miles: " + bonusMile);
+    }
+}
